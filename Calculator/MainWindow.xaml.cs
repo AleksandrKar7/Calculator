55d6470e72b9f5c9269d6b38ab2f8adf28db241f
AleksandrKar <AlreksandrKar7@gmail.com>
2019-10-12 23:20:18 +0300

    Interface added
    Project:
    1. Added the interface of the usual form of the calculator;
    2. Added input, without checking for correctness, of numbers from 0 to 9, as well as actions + - * /, as well as a separator;
    3. Implemented the Backspace button, and clear the input field.
    Tests:
    1. Added test to verify the character input method.

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -20,9 +20,124 @@ namespace Calculator
     /// </summary>
     public partial class MainWindow : Window
     {
+        private TextBlock main;
+        public TextBlock Main{get { return main; }}
+
+        private TextBlock second;
         public MainWindow()
         {
             InitializeComponent();
+            main = this.TB_Main;
+            second = this.TB_Second;
         }
+
+        private void Btn_Zero_Click(object sender, RoutedEventArgs e)
+        {
+            AddSympbolInTBMain('0');
+        }
+
+        private void Btn_One_Click(object sender, RoutedEventArgs e)
+        {
+            AddSympbolInTBMain('1');
+        }
+
+        private void Btn_Two_Click(object sender, RoutedEventArgs e)
+        {
+            AddSympbolInTBMain('2');
+        }
+
+        private void Btn_Three_Click(object sender, RoutedEventArgs e)
+        {
+            AddSympbolInTBMain('3');
+        }
+
+        private void Btn_Four_Click(object sender, RoutedEventArgs e)
+        {
+            AddSympbolInTBMain('4');
+        }
+
+        private void Btn_Five_Click(object sender, RoutedEventArgs e)
+        {
+            AddSympbolInTBMain('5');
+        }
+
+        private void Btn_Six_Click(object sender, RoutedEventArgs e)
+        {
+            AddSympbolInTBMain('6');
+        }
+
+        private void Btn_Seven_Click(object sender, RoutedEventArgs e)
+        {
+            AddSympbolInTBMain('7');
+        }
+
+        private void Btn_Eight_Click(object sender, RoutedEventArgs e)
+        {
+            AddSympbolInTBMain('8');
+        }
+
+        private void Btn_Nine_Click(object sender, RoutedEventArgs e)
+        {
+            AddSympbolInTBMain('9');
+        }
+
+
+        private void Btn_Divide_Click(object sender, RoutedEventArgs e)
+        {
+            AddSympbolInTBMain('/');
+        }
+
+        private void Btn_Multi_Click(object sender, RoutedEventArgs e)
+        {
+            AddSympbolInTBMain('*');
+        }
+
+        private void Btn_Minus_Click(object sender, RoutedEventArgs e)
+        {
+            AddSympbolInTBMain('-');
+        }
+
+        private void Btn_Plus_Click(object sender, RoutedEventArgs e)
+        {
+            AddSympbolInTBMain('+');
+        }
+
+        private char separator = '.';
+        public char Separator { get { return separator; } }
+        private void Btn_Dote_Click(object sender, RoutedEventArgs e)
+        {
+            AddSympbolInTBMain(separator);
+        }
+
+        private void Btn_Clear_Click(object sender, RoutedEventArgs e)
+        {
+            ClearAllInTB();
+        }
+
+        private void Btn_Backspace_Click(object sender, RoutedEventArgs e)
+        {
+            Backspace();
+        }
+
+        public void AddSympbolInTBMain(char ch)
+        {
+            main.Text += ch;
+        }   
+
+        private void ClearAllInTB()
+        {
+            main.Text = String.Empty;
+            second.Text = String.Empty;
+        }
+
+        private void Backspace()
+        {
+            if(main.Text.Length > 0){
+                main.Text = main.Text.Remove(main.Text.Length - 1);
+            }
+        }
+
+
+
     }
 }
---
+++ UnitTestForCalculator/Buttons.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Calculator;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestForCalculator
+{
+    [TestClass]
+    public class Buttons
+    {
+        [TestMethod]
+        public void AddSympbolInTBMain_AddNumbers_NumberExist()
+        {
+            //arrange
+            char[] forSend = { '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' };
+            string expected = "1234567890";
+            MainWindow main = new MainWindow();
+            string actual = null;
+            //act
+            foreach (char ch in forSend)
+            {
+                main.AddSympbolInTBMain(ch);
+                actual = main.Main.Text;
+            }
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+}
